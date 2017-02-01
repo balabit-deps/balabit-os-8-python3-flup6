1400df9125dbbf0d16215b7ca6716fcf55b4e895
Hercinger Viktor <viktor.hercinger@balabit.com>
2017-02-01 12:10:52 +0100

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'flup6',
-    version = '1.1.1',
+    version = '1.1.1-1ubuntu1',
     packages = find_packages(),
     zip_safe = True,
     
